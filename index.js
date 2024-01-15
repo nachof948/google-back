6af0b0243c20238561d5dc599aa4e88bf39c26ce
nachof948 <nachof948@gmail.com>
2024-01-15 15:12:10 -0300

    Segundo Commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(passport.session())
 
 app.use(
     cors({
-    origin: "https://localhost:3000",
+    origin: "http://localhost:3000",
     methods:"GET, POST, PUT, DELETE",
     credentials: true
 }))
